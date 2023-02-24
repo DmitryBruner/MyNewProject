baf57928bc5abc7b41f26f9cd679b4b66144f465
Bruner <bruner.expert@yandex.ru>
2023-02-24 06:44:51 +0300

    authenticate and feedback

--- mynewproject/women/forms.py
+++ mynewproject/women/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from captcha.fields import CaptchaField
 
 from .models import *
 
@@ -33,6 +34,17 @@ class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=255)
+    email = forms.EmailField(label='Email')
+    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 38, 'rows': 4}))
+    captcha = CaptchaField()
+
 
 #добавляем форму без привязки к модели
 # class AddPostForm(forms.Form):
--- mynewproject/women/views.py
+++ mynewproject/women/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from  django.contrib.auth.mixins import LoginRequiredMixin
 
 from .utils import *
@@ -122,11 +124,18 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
 #         form = AddPostForm()
 #     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
-def login(request):
-     return HttpResponse('Обратная связь')
+# def login(request):
+#      return HttpResponse('Обратная связь')
 
-def contact(request):
-    return HttpResponse('Авторизация')
+class ContactForView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwards):
+        context = super().get_context_data(**kwards)
+        def_c = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items())+list(def_c.items()))
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
@@ -143,9 +152,25 @@ class RegisterUser(DataMixin, CreateView):
         def_c = self.get_user_context(title='Регистрация')
         return dict(list(context.items())+list(def_c.items()))
 
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
 
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(title='Авторизация')
+        def_c = self.get_user_context(title='Авторизация')
+        return dict(list(context.items())+list(def_c.items()))
+    def get_success_url(self):
+        return reverse_lazy('home')
 
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 # пример использования регулярок
 # def archive(request, year):
 #     if int(year) > 2020:
